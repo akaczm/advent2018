5c56fb9107bc523dd509b963550df03343b5290a
unknown <akaczmarek@man.poznan.pl>
2018-12-03 11:02:52 +0100

    Fixed day 2 part 1

--- Day 2/day2.py
+++ Day 2/day2.py
@@ -3,16 +3,16 @@ import string
 
 done_dubs = []
 done_tris = []
+c = [0,0]
 
-with open('testinput.txt') as f:
+with open('input.txt') as f:
     for index, line in enumerate(f.readlines()):
         for char in string.ascii_lowercase:
             if line.count(char) == 2 and index not in done_dubs:
                 done_dubs.append(index)
-                print(f"Found dubs in index {index}")
+        for char in string.ascii_lowercase:
             if line.count(char) == 3 and index not in done_tris:
                 done_tris.append(index)
-                print(f"Found tris in index {index}")
                 break
             
     print(len(done_dubs) * len(done_tris))
